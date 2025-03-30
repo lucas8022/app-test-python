31c9a92f51bcd9e7424bb6f8070543cc0e458963
Lucas Felix <lucasfelix986@gmail.com>
2025-03-29 23:11:55 -0300

    Adicionamos a classe pessoa no projeto

---
+++ jogo.py
@@ -0,0 +1,8 @@
+class Pessoa(object):
+    def __init__(self, idade, nome, altura):
+        self.idade = idade
+        self.nome = nome
+        self.altura = altura
+
+    def andar():
+        pass
